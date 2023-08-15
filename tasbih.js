b1ca0631061bfbc8ad113917a31833960e33cef1
Rabiul37 <hmrabiul37@gmail.com>
2023-08-15 15:56:41 +0600

    this is the tasbih first commit

---
+++ tasbih.js
@@ -0,0 +1,6 @@
+document.getElementById('increse').addEventListener('click',function(){
+    let counter =document.getElementById('counter');
+    let sum = 0 ;
+    counter.innerText = sum+=;
+    
+})
